ba105db87d1d43caaab1a88224249465ed72d6e9
Juan Lorenzo <gllore@lsu.edu>
2021-06-05 12:15:03 -0500

    demo files are included as well as new programs

---
+++ demo_projects/demos/seismics/pl/Synthetic/Finite_Difference/Sufctanismod/Xdemo1/1/gllore/chain.sh
@@ -0,0 +1,13 @@
+ perl 1.make_vp_model.pl
+ perl 2.view_vp_model.pl
+ perl 3.make_vs_model.pl
+ perl 4.view_vs_model.pl
+ perl 5.make_rho_model.pl
+ perl 6.view_rho_model.pl
+ perl 7.make_delta_model
+ perl 8.view_delta_model.pl
+ perl 9.make_epsilon_model.pl
+ perl 10.view_epsilon_model.pl
+ perl 11.make_dummy_zero_model.pl
+ perl 12.view_dummy_zero_model.pl
+ perl 13.transform2stiffness.pl
