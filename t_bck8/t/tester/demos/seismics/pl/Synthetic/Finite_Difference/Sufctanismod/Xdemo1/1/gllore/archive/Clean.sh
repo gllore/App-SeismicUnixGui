0a9e87fe170dd4bbff4becf1e69344f0934f6c63
Juan Lorenzo <gllore@lsu.edu>
2022-06-09 13:29:22 -0500

    Separated demos into another repository
    New github repository is L_SU_demos
    The main repository is now focused on code

--- t_bck8/t/tester/demos/seismics/pl/Synthetic/Finite_Difference/Sufctanismod/Xdemo1/1/gllore/archive/Clean.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/sh
-# Clean.sh created data files
-
-rm -f *.out junk* *.dir *.data *.eps *.ps density* velocity*  *reflection*
-
-exit
--- t_bck8/tester/demos/seismics/pl/shapeNcut/sumute/gllore/under_construction/Clean.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-rm -rf .temp*
-rm -rf .split*
-rm -rf .tx*
-rm -rf log.txt
-rm mute_multi_gather_file
