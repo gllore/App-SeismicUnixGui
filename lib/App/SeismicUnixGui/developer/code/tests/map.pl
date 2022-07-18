4aff2e0ec0e6a443205274477e66b3a4731bb675
Juan Lorenzo <gllore@lsu.edu>
2022-07-18 15:31:10 -0500

    testing .gitignore

---
+++ lib/App/SeismicUnixGui/developer/code/tests/map.pl
@@ -0,0 +1,14 @@
+
+
+my $defstartup = 'default.perldlrc';
+my @pm_names = qw ( PDL.pm Lite.pm LiteF.pm AutoLoader.pm Options.pm
+                    Matrix.pm Reduce.pm Lvalue.pm Constants.pm);
+my %pm = map { my $h = '$()/';
+               $h .= 'PDL/' if $_ !~ /PDL.pm$/;
+               ( $_, $h . $_ );
+           } ( @pm_names, $defstartup );
+           
+ 
+           foreach my $key ( keys %pm) {
+              print $key."\n";
+           };
