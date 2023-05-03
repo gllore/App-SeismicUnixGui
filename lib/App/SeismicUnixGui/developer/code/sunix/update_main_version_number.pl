a99d6deab6eddbc9ea51777981369f5ceab31580
Juan Lorenzo <gllore@lsu.edu>
2023-05-03 18:24:34 -0500

    Installation of C- and fortran-files
    Recommend env. variables to user

--- lib/App/SeismicUnixGui/developer/code/sunix/update_main_version_number.pl
+++ lib/App/SeismicUnixGui/developer/code/sunix/update_main_version_number.pl
@@ -32,83 +32,110 @@ use Cwd;
 
 my $manage_files_by2 = manage_files_by2->new();
 
-my (@file,@file_bck);
+my ( @file, @file_bck );
 my $path;
 my @path2file;
 my @inbound;
 my @outbound_bck;
 my @outbound;
 my ( @line2find, @replacement );
+my $max_num_files = 4;
 
-my $up3dirs = '/../../../';
-
+my $up3dirs = '/../../..';
+my $up4dirs = '/../../../..';
 
 =head2 set replacement text
 and file
 
 =cut
 
-my $local   = getcwd();
+my $local       = getcwd();
+my $old_version = '0.82.6';
+my $new_version = '0.82.7';
 
-my $i=0;
-$line2find[$i]    = 'L_SUV0.82.6.pl';
-$replacement[$i]  = 'L_SUV0.82.7.pl';
-$file[$i]         = 'L_SU.pl';
-$path2file[$i]    =  $local . $up3dirs . 'script';
+my $i = 0;
+$line2find[$i]   = ("L_SUV$old_version.pl");
+#print("line2find   = $line2find[$i]\n");
+$replacement[$i] = ("L_SUV$new_version.pl");
+#print("replacement = $replacement[$i]\n");
+$file[$i]        = 'L_SU.pl';
+$path2file[$i]   = $local . $up3dirs . '/script';
 
-$i=1;
-$line2find[$i]   = "\$VERSION = '0.82.6';";
-$replacement[$i] = "\$VERSION = '0.82.7';";
-$file[$i]        = 'About.pm';
-$path2file[$i]   =  $local . $up3dirs . 'messages';
+$i = 1;
+$line2find[$i] = "VERSION = '$old_version';";
+#print("line2find   = $line2find[$i]\n");
+$replacement[$i] = "VERSION = '$new_version';";
+#print("replacement = $replacement[$i]\n");
+$file[$i]      = 'About.pm';
+$path2file[$i] = $local . $up3dirs . '/messages';
+
+$i = 2;
+$line2find[$i] =
+  "_program_title                 => 'SeismicUnixGui V$old_version',";
+#print("line2find   = $line2find[$i]\n");
+$replacement[$i] =
+  "_program_title                 => 'SeismicUnixGui V$new_version',";
+#print("replacement = $replacement[$i]\n");
+$file[$i]      = 'L_SU_global_constants.pm';
+$path2file[$i] = $local . $up3dirs . '/misc';
+
+$i = 3;
+$line2find[$i] = "VERSION = '$old_version';";
+#print("line2find   = $line2find[$i]\n");
+$replacement[$i] = "VERSION = '$new_version';";
+#print("replacement = $replacement[$i]\n");
+$file[$i]      = 'SeismicUnixGui.pm';
+$path2file[$i] = $local . $up4dirs;
 
 =head2 Set files and paths
 
 =cut
 
+for ( my $count = 0 ; $count < $max_num_files; $count++ ) {
 
-$i=1;
+	$file_bck[$count] = $file[$count] . '_bck';
+	
+	$inbound[$count]      = $path2file[$count] . '/' . $file[$count];
+	$outbound[$count]     = $path2file[$count] . '/' . $file[$count];
+	$outbound_bck[$count] = $path2file[$count] . '/archive/' . $file_bck[$count];
 
-$file_bck[$i] = $file[$i].'_bck';
+	# save a backup file
+	system("cp $inbound[$count] $outbound_bck[$count]");
 
+	# slurp every file
+	$manage_files_by2->set_pathNfile( $inbound[$count] );
+	my $slurp_ref = manage_files_by2->get_whole();
 
-$inbound[$i]      = $path2file[$i] . '/' . $file[$i];
-$outbound[$i]     = $path2file[$i] . '/' . $file[$i];
-$outbound_bck[$i] = $path2file[$i] . '/archive/' . $file_bck[$i];
+	my @slurp           = @$slurp_ref;
+	my $length_of_slurp = scalar @slurp;
 
-# save a backup file
-system("cp $inbound[$i] $outbound_bck[$i]");
+	for ( my $line_idx = 0 ; $line_idx < $length_of_slurp ; $line_idx++ ) {
 
-# slurp every file
-$manage_files_by2->set_pathNfile( $inbound[$i] );
-my $slurp_ref = manage_files_by2->get_whole();
+		# CASE within each *.pm file
+		my $string = $slurp[$line_idx];
 
-my @slurp           = @$slurp_ref;
-my $length_of_slurp = scalar @slurp;
+		#  chomp $string;    # remove all newlines
+		if ( $string =~ m/$line2find[$count]/) {
+			$string =~ s/$line2find[$count]/$replacement[$count]/;
+			print(" substitution successful: $string\n");
+			print(" in file: $file[$count]\n");
+		}
 
-for ( my $line_idx = 0 ; $line_idx < $length_of_slurp ; $line_idx++ ) {
+#		print(" string: $string\n");
+		$slurp[$line_idx] = $string;
 
-	# CASE within each *.pm file
-	my $string = $slurp[$line_idx];
-
-	#  chomp $string;    # remove all newlines
-	if ( $string =~ m/$line2find[0]/ ) {
-		$string =~ s/$line2find[0]/$replacement[0]/;
-		print(" substitution successful: $string\n");
 	}
 
-	$slurp[$line_idx] = $string;
+	open( OUT, ">$outbound[$count]" )
+	  or die("File $outbound[$count] not found");
+
+	foreach my $text (@slurp) {
+
+		printf OUT $text . "\n";
+
+		#	print("$text \n");
+	}
+	close(OUT);
 
 }
-
-open( OUT, ">$outbound[$i]" )
-  or die("File $outbound[$i] not found");
-
-foreach my $text (@slurp) {	
-	
-	printf OUT $text."\n";
-
-#	print("$text \n");
-}
-close(OUT);
 
