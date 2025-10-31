39e722647d0e937e0bc4272fc2ec694374809289
Juan Lorenzo <gllore@lsu.edu>
2025-10-31 17:18:09 -0500

    Corrected merging with old branch

--- lib/App/SeismicUnixGui/misc/L_SU.pm
+++ lib/App/SeismicUnixGui/misc/L_SU.pm
@@ -1994,7 +1994,6 @@ sub user_built_flows {
 						# print("1993 L_SU,user_built_flows, pink,print out gui_history\n");
 						# $gui_history->view();
 
-<<<<<<< HEAD
 						# if ($most_recent_button eq 'superflow_select_button') {
 
 							# print("1997 L_SU,user_built_flows, pink, last selection was not a superflow\n");
@@ -2006,18 +2005,18 @@ sub user_built_flows {
 						# 	print("2002 L_SU,user_built_flows, pink, last selection was a pre-built superflow\n");
 						# 	#NADA
 						# }
-=======
-						if ($most_recent_button eq 'superflow_select_button') {
-
-							# print("1997 L_SU,user_built_flows, pink, last selection was not a superflow\n");
-							# print("1997 L_SU,user_built_flows, pink, last selection was a user-built flow item\n");											
-							$pink_flow->flow_select2save_most_recent_param_flow();
-
-						} else {
-							print("2002 L_SU,user_built_flows, pink, last selection was a pre-built superflow\n");
-							#NADA
-						}
->>>>>>> origin/V0.87.5
+#=======
+#						if ($most_recent_button eq 'superflow_select_button') {
+#
+#							# print("1997 L_SU,user_built_flows, pink, last selection was not a superflow\n");
+#							# print("1997 L_SU,user_built_flows, pink, last selection was a user-built flow item\n");											
+#							$pink_flow->flow_select2save_most_recent_param_flow();
+#
+#						} else {
+#							print("2002 L_SU,user_built_flows, pink, last selection was a pre-built superflow\n");
+#							#NADA
+#						}
+#>>>>>>> origin/V0.87.5
 
 					}
 					else {
